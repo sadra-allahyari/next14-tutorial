094defb37a7cd9c6bc3b6e44b57ce428036639f1
Sadra Allahyari <ms.allahyari.m@gmail.com>
2024-06-01 15:38:40 +0330

    Add images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,12 @@
+import Image from "next/image"
+import styles from "./about.module.css"
+
 const AboutPage = () => {
     return (
-        <div>AboutPage</div>
+        <div>
+            <div className={styles.imgContainer}/>
+            <Image src="/about.png" alt="" width={500} height={500}/>
+        </div>
     )
 }
 
